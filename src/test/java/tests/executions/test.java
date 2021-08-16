d3890644ee0a2430121c33525bdb857654f182b2
Teguh Hariyadi <teguh.hariyadi@mitrais.com>
2021-08-16 21:34:13 +0700

    add slide and accept alert case

--- src/test/java/tests/executions/test.java
+++ src/test/java/tests/executions/test.java
@@ -1,5 +1,6 @@
 package tests.executions;
 
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.touch.offset.PointOption;
@@ -102,6 +103,8 @@ public class test {
         driver.findElement(By.id("com.experitest.ExperiBank:id/sendPaymentButton")).click();
         //driver.findElement(By.id("android:id/button1")).click();
         driver.switchTo().alert().accept();
+        driver.findElementByAccessibilityId("");
+        driver.f
     }
 
 }
